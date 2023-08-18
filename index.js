ecabc196c103bccb7842a9259f461f447eea8212
Atif <atif@monotote.com>
2023-08-18 17:21:12 +0500

    WIP

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(notFound);
+app.use(notFound);
 app.use(handleError)
 
 //routes
